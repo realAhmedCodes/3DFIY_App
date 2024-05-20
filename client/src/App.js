e693b5e56a072fadd506a4e85da98312ae4a6fdc
realAhmedCodes <ahmedsiddque576@gmail.com>
2024-05-20 22:53:15 +0500

    Import added

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import { User_SignUp } from './pages/User_SignUp';
 
 function App() {
   return (
     <div className="App">
-      
+      <User_SignUp></User_SignUp>
     </div>
   );
 }
