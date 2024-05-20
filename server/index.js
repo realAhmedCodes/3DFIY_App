7750d81dc07289c61240f7f6dcce4c5e6ca19d6b
realAhmedCodes <ahmedsiddque576@gmail.com>
2024-05-20 23:03:53 +0500

    code updation

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const express = require("express")
 
 const cors = require("cors")
 const app= express()
-const bcrypt= require("bycrpt")
+const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken")
 
 //Routes imports
