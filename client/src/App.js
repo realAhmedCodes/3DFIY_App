5e051daf3866f43b3dbbf2acc45fdf1fa467546a
realAhmedCodes <ahmedsiddque576@gmail.com>
2024-05-24 02:59:22 +0500

    Pages Imports

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,28 @@
 
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { User_SignUp } from './pages/User_SignUp';
 import { Model_Upload } from './pages/Model_Upload';
-
+import { Login } from './pages/Login';
 function App() {
   return (
     <div className="App">
-     
-      <Model_Upload></Model_Upload>
-      
+      <Router>
+        <Routes>
+          <Route
+            path="/User_SignUp"
+            element={<User_SignUp></User_SignUp>}
+          ></Route>
+          <Route
+            path="/Model_Upload"
+            element={<Model_Upload></Model_Upload>}
+          ></Route>
+          <Route
+            path="/Login"
+            element={<Login></Login>}
+          ></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
