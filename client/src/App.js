ec5d0ed09e6ffa6c5caaa22a90a480879b9fa87d
realAhmedCodes <ahmedsiddque576@gmail.com>
2024-05-24 02:15:17 +0500

    Model component import

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 
 import './App.css';
 import { User_SignUp } from './pages/User_SignUp';
+import { Model_Upload } from './pages/Model_Upload';
 
 function App() {
   return (
     <div className="App">
-      <User_SignUp></User_SignUp>
+     
+      <Model_Upload></Model_Upload>
+      
     </div>
   );
 }
