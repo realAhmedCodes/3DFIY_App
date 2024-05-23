046b61dc21b27e610163e8221a67a9e5f0d79c5d
realAhmedCodes <ahmedsiddque576@gmail.com>
2024-05-24 04:44:32 +0500

    Login Form

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,49 +1,20 @@
 import React, { useState, useEffect } from "react";
 import styles from "../styles/Auth.module.css";
 import { useCookies } from "react-cookie";
-import { Navbar } from "../component/Navbar";
+
+
 import { jwtDecode, InvalidTokenError } from "jwt-decode"; // Import jwtDecode instead of jwt_decode
-import { addUserValues } from "../slices/userValues";
-import { addJobInfo } from "../slices/Job_Info";
-import { useDispatch } from "react-redux";
-import icon from "../icons/icon.png";
+
+
+
 
 export const Login = () => {
-  const dispatch = useDispatch();
+ 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState(null);
   const [cookies, setCookie, removeCookie] = useCookies(null);
   const [check, setCheck] = useState("");
-
-  /*useEffect(() => {
-    const storeData= async()=>{
-      const token = window.sessionStorage.getItem("token");
-      try {
-        if (token) {
-          const decodedToken = jwtDecode(token);
-          const userEmail = decodedToken.email;
-          const userRole = decodedToken.role;
-          const userId = decodedToken.user_id;
-          console.log(userEmail, userId);
-          setCheck(token);
-          const userValues = {
-            userEmail,
-            userRole,
-            userId,
-          };
-
-          dispatch(addUserValues(userValues));
-        }
-      } catch (error) {
-        if (error instanceof InvalidTokenError) {
-          // Handle invalid token error
-          console.error("Invalid token");
-        }
-      }
-    }
-    storeData()
-  }, []); // Run this effect only once on component mount*/
 
   const submitBtn = async (e) => {
     e.preventDefault();
@@ -67,10 +38,10 @@ export const Login = () => {
   console.log(check);
   return (
     <div class="flex min-h-full flex-col justify-centerlg">
-      <Navbar></Navbar>
+     
 
       <div class="sm:mx-auto sm:w-full sm:max-w-sm mt-24">
-        <img class="mx-auto h-16 w-auto" src={icon} alt="Your Company" />
+       
         <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
           Sign in to your account
         </h2>
