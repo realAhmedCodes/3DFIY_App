52d8f75c88a91108cdb15e67084e1518f0b428da
realAhmedCodes <ahmedsiddque576@gmail.com>
2024-05-25 21:59:23 +0500

    View Model Page added

---
+++ client/src/pages/ViewModel.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from 'react'
+
+export const ViewModel = () => {
+    const[models, setModels]= useState([])
+
+
+      useEffect(() => {
+        const fetchData = async () => {
+          try {
+            const response = await fetch(
+              `http://localhost:8000/modelApi/models`
+            );
+
+            const modelsData = await response.json();
+            setModels(modelsData);
+          } catch (err) {
+            console.log(err);
+          }
+        };
+
+        fetchData();
+      }, []);
+      console.log(models)
+  return (
+    <div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
+        <h1 className="text-xl font-bold mb-2">Latest Models</h1>
+        {models.map((model) => (
+          <div
+            key={model.model_id}
+           
+            className="border rounded-md p-2 cursor-pointer hover:bg-gray-200"
+          >
+            <p>{model.name}</p>
+            <p>{model.description}</p>
+            <p>{model.price}</p>
+            
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
