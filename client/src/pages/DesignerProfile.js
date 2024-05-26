4b13b381d8f44bce4724752b0c26f486509bcbbc
realAhmedCodes <ahmedsiddque576@gmail.com>
2024-05-27 04:30:14 +0500

    Profile code changes

--- client/src/pages/DesignerProfile.js
+++ client/src/pages/DesignerProfile.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from "react";
-import { jwtDecode } from "jwt-decode"; // Ensure this import is correct
+import {jwtDecode} from "jwt-decode"; // Ensure this import is correct
+import { useNavigate } from "react-router-dom";
 
 export const DesignerProfile = () => {
   const [designer, setDesigner] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-  const [designerId, setDesignerId] = useState(null); 
+  const [designerId, setDesignerId] = useState(null);
+  const [models, setModels] = useState([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const token = window.sessionStorage.getItem("token");
@@ -19,12 +22,27 @@ export const DesignerProfile = () => {
       }
     }
   }, []);
-  console.log(designerId);
 
-  
+  useEffect(() => {
+    if (!designerId) return;
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          `http://localhost:8000/modelApi/models/${designerId}`
+        );
+        const modelsData = await response.json();
+        setModels(modelsData);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    fetchData();
+  }, [designerId]);
+
   useEffect(() => {
     const fetchData = async () => {
-      if (!designerId) return; 
+      if (!designerId) return;
 
       try {
         const response = await fetch(
@@ -43,7 +61,7 @@ export const DesignerProfile = () => {
     };
 
     fetchData();
-  }, [designerId]); 
+  }, [designerId]);
 
   if (loading) {
     return (
@@ -68,35 +86,55 @@ export const DesignerProfile = () => {
       </div>
     );
   }
-console.log(designer.profile_pic)
+
+  console.log(designer.profile_pic);
   return (
     <div className="container mx-auto px-4 py-8">
-      <div className="bg-white shadow-lg rounded-lg p-6 mb-6">
-        <div className="flex items-center">
-          <div className="flex-grow">
-            <div className="flex items-center mb-4">
-              <div>
-                <img
-                  src={`http://localhost:8000/uploads/${designer.profile_pic
-                    .split("\\")
-                    .pop()}`}
-                  alt={designer.name}
-                  className="w-16 h-16 rounded-full mr-4"
-                />
-              </div>
-              ;
-              <div className="flex flex-col">
-                <h2 className="text-2xl font-bold">{designer.name}</h2>
-                <p className="text-gray-600">{designer.location}</p>
-              </div>
-            </div>
-            <p className="italic text-gray-700">{designer.bio}</p>
-          </div>
+      <div className="bg-white shadow-lg rounded-lg p-6 mb-6 flex items-center">
+        <div className="mr-4">
+          {designer.profile_pic && (
+            <img
+              src={`http://localhost:8000/uploads/${designer.profile_pic
+                .split("\\")
+                .pop()}`}
+              alt={designer.name}
+              className="w-16 h-16 rounded-full mr-4"
+            />
+          )}
         </div>
+
+        <div className="flex-grow">
+          <div className="flex flex-col">
+            <h2 className="text-2xl font-bold">{designer.name}</h2>
+            <p className="text-gray-600">{designer.location}</p>
+          </div>
+          <p className="italic text-gray-700 mt-2">{designer.bio}</p>
+        </div>
+      </div>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
+        <h1 className="text-xl font-bold mb-2">Latest Models</h1>
+        {models.map((model) => (
+          <div
+            key={model.model_id}
+            className="border rounded-md p-2 cursor-pointer hover:bg-gray-200"
+            onClick={() => navigate(`/model/${model.model_id}`)}
+          >
+            <p>{model.name}</p>
+            <p>{model.price}</p>
+            {console.log(model.image)}
+            {model.image && (
+              <img
+                src={`http://localhost:8000/uploads/models/${model.image
+                  .split("\\")
+                  .pop()}`}
+                alt={model.name}
+                className="w-full h-auto"
+              />
+            )}
+          </div>
+        ))}
       </div>
     </div>
   );
 };
-
-
-
